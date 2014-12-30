8e0e887d26a75e6dc89bda79a481d60e7409e01f
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2014-12-31 01:15:22 +0300

    0.0.1 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='mcrowd-platform',
-      version='0.0.1',
+      version='0.0.2',
       description="Mechanical Crowd Platform",
       author='Kirill Goldshtein',
       author_email='goldshtein.kirill@gmail.com',
