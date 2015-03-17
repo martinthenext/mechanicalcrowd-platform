02217c0c0e35b041d58760e3f517e6e5770f364a
Kirill Goldshtein <kirill@goldshtein.org>
2015-03-17 23:39:40 +0300

    add simple classes

---
+++ addin/addin/Table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace addin
+{
+    class Table
+    {
+        Excel.ListObject table;
+
+        public Table(Excel.ListObject table) 
+        {
+            this.table = table;
+        }
+    }
+}
