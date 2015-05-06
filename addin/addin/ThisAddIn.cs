b71c4db2f1ef7548b0e54a00e0f5213ec8cfd75f
Kirill Goldshtein <kirill@goldshtein.org>
2015-05-07 00:51:15 +0300

    sending event from excel to anybody via zmq

--- addin/addin/ThisAddIn.cs
+++ addin/addin/ThisAddIn.cs
@@ -6,17 +6,58 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using ZMQ = ZeroMQ;
 
 namespace addin
 {
     public partial class ThisAddIn
     {
+        private ZMQ.ZContext ctx;
+        private ZMQ.ZSocket client;
+
+        private void InitZmqClient() 
+        {
+            ctx = new ZMQ.ZContext();
+            client = new ZMQ.ZSocket(ctx, ZMQ.ZSocketType.REQ);
+            client.Connect("tcp://127.0.0.1:8080");
+        }
+
+        private void DestroyZmqClient() 
+        {
+            client.Close();
+            ctx.Shutdown();
+        }
+
+        private String SendEvent(object sender, System.EventArgs e) 
+        {
+            return SendEvent(sender.ToString() + e.ToString());
+        }
+
+        private String SendEvent(String msg)
+        {
+            client.Send(new ZMQ.ZFrame(msg));
+            using (ZMQ.ZFrame reply = client.ReceiveFrame())
+            {
+                return reply.ReadString();
+            }
+        }
+
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            InitZmqClient();
+            SendEvent(sender, e);
+            this.Application.WorkbookOpen += new Excel.AppEvents_WorkbookOpenEventHandler(ThisAddIn_WorkbookOpen);
+        }
+
+        void ThisAddIn_WorkbookOpen(Excel.Workbook Wb)
+        {
+            SendEvent("Open: " + Wb.FullName);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+            SendEvent(sender, e);
+            DestroyZmqClient();
         }
 
         #region Код, автоматически созданный VSTO
