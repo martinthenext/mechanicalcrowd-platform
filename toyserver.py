c6c83ab75b012596acfbeb11f647ff4cbf1e049d
Martin Davtyan <martin.davtyan@gmail.com>
2015-05-21 00:18:24 +0200

    After sending a requiest from C# excel is actually blocked before the response comes in

--- toyserver.py
+++ toyserver.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python2
 import zmq
+import time
 
-
-def main():
+def main(wait=False):
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://127.0.0.1:8080")
     while True:
         message = socket.recv()
-        print "Received request: ", message 
-        socket.send("OK")
+        print "Received request: ", message
+        
+        if wait:
+            print "Waiting 10 seconds before responding"
+            time.sleep(10)
+            print "Response sent"
 
+        socket.send("OK")
 
 if __name__ == '__main__':
     main()
