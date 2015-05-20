3d7effd1a96de62af8707b662bf426921654d263
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2015-05-21 01:19:48 +0300

    fix router

--- zmq/router.py
+++ zmq/router.py
@@ -10,18 +10,19 @@ def worker(context):
     sock = context.socket(zmq.REQ)
     sock.setsockopt(zmq.IDENTITY, ident)
     sock.connect("inproc://backend")
-    
+
     data = ident
     while True:
         sock.send(data)
         event = json.loads(sock.recv())
         client = event["client"]
         payload = event["payload"]
-        print "Worker '%s' receive from client '%s' payload: %s" (ident, client, payload)
+        print "Worker '%s' receive from client '%s' payload: %s" % \
+            (ident, client, payload)
         # do something with payload and return result
         data = json.dumps({"client": client, "payload": "A1"})
 
-        
+
 def main():
     context = zmq.Context()
     frontend = context.socket(zmq.ROUTER)
@@ -31,9 +32,9 @@ def main():
 
     workers = {}
     worker_count = 10
-    
+
     frontends = {}
-    
+
     poller = zmq.Poller()
     poller.register(backend, zmq.POLLIN)
     poller.register(frontend, zmq.POLLIN)
@@ -46,15 +47,17 @@ def main():
                 workers[ident] = True
                 print "worker '%s' is registered" % ident
             else:
+                event = json.loads(event)
                 client = event["client"]
                 payload = event.get("payload")
                 if payload:
-                    print "sending payload to client '%s': %s" % (client, payload)
-                    frontend.send_multipart(client, "", event["payload"])
+                    print "sending payload to client '%s': %s" % \
+                        (client, payload)
+                    frontend.send_multipart([str(client), "", str(payload)])
                 else:
                     print "nothing to send to client '%s'" % client
                 workers[ident] = True
-        
+
     def frontend_routine(socks, available):
         if frontend in socks and socks[frontend] == zmq.POLLIN:
             ident, _, payload = frontend.recv_multipart()
@@ -62,22 +65,24 @@ def main():
                 # initializing frontends
                 frontends[ident] = True
                 print "long polling request from '%s'" % ident
-            elif ident.startswith("event"): # from excel to python
+            elif ident.startswith("event"):  # from excel to python
                 print "event request from '%s'" % ident
                 client = "async-" + ident.split("-")[1]
                 if frontends.get(client):
                     event = json.dumps({"client": client, "payload": payload})
-                    print "sending to worker '%s' payload: %s" % (available, event)
-                    backend.send_multipart(available, "", event)
+                    print "sending to worker '%s' payload: %s" % \
+                        (available, event)
+                    backend.send_multipart([str(available), "", str(event)])
                     workers[available] = False
                     frontends[client] = False
                 else:
-                    print "could not send payload to worker: %s" % payload
-                frontend.send_multipart(ident, "", "OK") # fast answer
-    
+                    print ("client '%s' is not available, "
+                           "could not send payload: %s") % (client, payload)
+                frontend.send_multipart([str(ident), "", "OK"])  # fast answer
+
     for _ in xrange(worker_count):
         threading.Thread(target=worker, args=(context,)).start()
-        
+
     while True:
         socks = dict(poller.poll())
         backend_routine(socks)
